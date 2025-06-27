8a46d31ea4c85be9473e0f838b84fb876e9d4cea
BradleyDuski <duskibrad@gmail.com>
2025-06-26 22:01:45 -0500

    First class

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import './App.css'
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/footer.jsx';
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <div className='main-content'>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      <h1>Hello World from React</h1>
+      </div>
+      <Footer/>
+     
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+function Footer(){
+return(
+
+<div className="foot">
+<h3>Created by: Bradley_Duski</h3>
+</div>
+);
+}
+
+
+
+export default Footer;
